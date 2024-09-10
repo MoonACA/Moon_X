5c1ddeef75405c0445fa7d10e3615bb456684338
Etiosa <115146462+sir-mills@users.noreply.github.com>
2024-09-10 17:01:05 -0600

    Update App.tsx
    added the chain config

--- app/App.tsx
+++ app/App.tsx
@@ -2,16 +2,25 @@
 
 import '@rainbow-me/rainbowkit/styles.css';
 import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import { mainnet, polygon, optimism, arbitrum, base } from 'wagmi/chains';
+import { mainnet } from 'wagmi/chains';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import React, { ReactNode } from 'react';
 import { WagmiProvider } from 'wagmi';
 const queryClient = new QueryClient();
 
+const mande = {
+  id: 18071918,
+  name: "Mande",
+  nativeCurrency: { name: "Mande", symbol: "MAND", decimals: 18 },
+  rpcUrls: {
+    default: { http: ["https://mande-mainnet.public.blastapi.io"] },
+  },
+} as const satisfies Chain;
+
 const config = getDefaultConfig({
-  appName: 'My RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
-  chains: [mainnet, polygon, optimism, arbitrum, base],
+  appName: 'moonx',
+  projectId: 'b52456c0ffdabb5678905aae9f2aa217',
+  chains: [mainnet, mande],
   ssr: true, // If your dApp uses server-side rendering (SSR)
 });
 
