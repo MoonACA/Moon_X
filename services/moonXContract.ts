334202e109ed809f9e92eb954ac65af7f649059e
Knowledge-JO <megamindtheincredible@gmail.com>
2024-10-07 13:00:57 +0100

    added value

--- services/moonXContract.ts
+++ services/moonXContract.ts
@@ -1,6 +1,6 @@
 import { Config } from "wagmi";
 import { WriteContractMutate } from "wagmi/query";
-
+import { parseEther } from "viem";
 import decentralLearningABI from "../abi/decentralLarning.json";
 
 const moonXContractAddress = "0x099551b20834FF484f83925700e4411348258328";
@@ -15,6 +15,7 @@ async function createCourse(
       abi: decentralLearningABI.output.abi,
       functionName: "createCourse",
       args: [args.metadataUri],
+      value: parseEther("0.75"),
     });
   } catch (error) {
     console.log("Error create course - createCourse()", error);
