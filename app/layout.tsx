ebbb800f3a8ef7e9902f93e5436afa8abefed5cf
Etiosa <115146462+sir-mills@users.noreply.github.com>
2024-10-08 03:59:13 -0600

    ssr: fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
-import type { Metadata } from 'next';
-
-import './globals.css';
-import App from './App';
+import type { Metadata } from "next";
+import "./globals.css";
+//import App from "./App";
+import dynamic from "next/dynamic";
 
 export const metadata: Metadata = {
-  title: 'Moon X',
-  description: 'Future of Blockchain',
+  title: "Moon X",
+  description: "Future of Blockchain",
 };
-
+const App = dynamic(() => import("./App"), { ssr: false });
 export default function RootLayout({
   children,
 }: Readonly<{
