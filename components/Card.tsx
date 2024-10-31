bcaead95275fc4f34020629b0dc80d98effc6f1f
kojusola <adeola5678@gmail.com>
2024-10-31 17:03:45 +0000

    feat: add enroll button to course card

--- components/Card.tsx
+++ components/Card.tsx
@@ -13,29 +13,35 @@ interface CourseType {
     rating: number;
     viewers: number;
   };
+  enroll?: boolean;
 }
 
-const Card: React.FC<CourseType> = ({ course }) => {
+const Card: React.FC<CourseType> = ({ course, enroll }) => {
   return (
     <div>
-      <div className=" bg-[#192A41] border rounded-xl border-[#FFFFFF] p-[1rem] grid gap-2">
+      <div className=' bg-[#192A41] border rounded-xl border-[#FFFFFF] p-[1rem] grid gap-2'>
         <Link href={`courses/${course?.id.toLocaleString()}`}>
-          <div className=" relative w-full h-[10rem] mx-auto z-[0] mb-[0.5rem]">
+          <div className=' relative w-full h-[10rem] mx-auto z-[0] mb-[0.5rem]'>
             <Image
               src={course?.thumbnail}
-              layout="fill"
-              objectFit="cover"
-              alt=""
-              className=" rounded-t-xl"
+              layout='fill'
+              objectFit='cover'
+              alt=''
+              className=' rounded-t-xl'
             />
           </div>
 
-          <h3 className=" text-[1rem] text-[#FFFFFF] font-medium h-[3rem]">
+          <h3 className=' text-[1rem] text-[#FFFFFF] font-medium h-[3rem]'>
             {course?.title}
           </h3>
 
-          <p className=" text-[#aaaaaa] text-[12px]">{course?.description}</p>
-          <div className=" flex justify-between items-center"></div>
+          <p className=' text-[#aaaaaa] text-[12px]'>{course?.description}</p>
+          {enroll && (
+            <button className='w-full mt-4 flex transition ease-in-out hover:duration-300 justify-center hover:opacity-80 items-center cursor-pointer py-1 font-semibold bg-[#FFEEE8] text-[#FF6636] text-[14px] leading-[40px]'>
+              Enroll Course
+            </button>
+          )}
+          <div className=' flex justify-between items-center'></div>
         </Link>
       </div>
     </div>
--- components/Courses.tsx
+++ components/Courses.tsx
@@ -8,21 +8,23 @@ const Courses = () => {
   const { courses, isLoading, error } = useCourses();
 
   return (
-    <div className=" ">
-      <div className=" text-white mb-[2rem]">
-        <h1 className=" text-[2rem] font-bold mb-[0.5rem] cursor-pointer">
+    <div className=' '>
+      <div className=' text-white mb-[2rem]'>
+        <h1 className=' text-[2rem] font-bold mb-[0.5rem] cursor-pointer'>
           Master the Future of the Internet with Web3 Tools
         </h1>
-        <p className=" text-sm w-[30vw]">
+        <p className=' text-sm w-[30vw]'>
           Comprehensive Courses on Blockchain, Smart Contracts, and Emerging
           Technologies
         </p>
       </div>
 
-      <h1 className="text-[2rem] text-[#fff] font-bold  mb-[2rem]">Courses</h1>
-      <div className=" grid grid-cols-4 mx-auto gap-3 max-xl:grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 ">
+      <h1 className='text-[2rem] text-[#fff] font-bold  mb-[2rem]'>Courses</h1>
+      <div className=' grid grid-cols-4 mx-auto gap-3 max-xl:grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 '>
         {courses &&
-          courses.map((course, index) => <Card course={course} key={index} />)}
+          courses.map((course, index) => (
+            <Card course={course} enroll={true} key={index} />
+          ))}
       </div>
     </div>
   );
