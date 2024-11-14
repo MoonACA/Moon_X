f6189ea405c718fc2e82faff8c1b2003eaa3c763
Etiosa Iyere <etiosaiyere@Etiosas-MacBook-Pro.local>
2024-11-14 13:32:30 +0100

    dev_push
    new_commit

--- supabase.ts
+++ supabase.ts
@@ -1,9 +1,8 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const superbaseUrl : any = process.env.NEXT_PUBLIC_SUPABASE_URL
-const superbaseKey: any = process.env.NEXT_PUBLIC_SUPABASE_KEY
+const superbaseUrl: any = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const superbaseKey: any = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 
-// Create a single supabase client for interacting with your database
-const supabase = createClient(superbaseUrl, superbaseKey)
+const supabase = createClient(superbaseUrl, superbaseKey);
 
-export default supabase
+export default supabase;
