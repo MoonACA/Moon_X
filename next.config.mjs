d67cae0441a7907d5e23fced2fc7e720d5fa5b9e
Knowledge-JO <megamindtheincredible@gmail.com>
2024-11-29 11:52:51 +0100

    bug fix

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,13 @@ const nextConfig = {
   images: {
     domains: ["bakurgmtpedjcnadobmy.supabase.co"],
   },
+  experimental: {
+    serverComponentsExternalPackages: [
+      "@aws-sdk/client-s3",
+      "@aws-sdk/lib-storage",
+      "aws-sdk",
+    ],
+  },
 };
 
 export default nextConfig;
